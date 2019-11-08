8a933ebc45cb65611094cfc60372fd5eb5bc559e
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-11-08 10:31:29 -0600

    add packages for redux, set up redux Provider

--- smurfs/src/actions/App.js
+++ smurfs/src/actions/App.js
@@ -2,7 +2,7 @@ export const FETCH_SMURF_LOADING = 'FETCH_SMURF_LOADING';
 export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
 export const FETCH_SMURF_FAILED = 'FETCH_SMURF_FAILED';
 
-export const smurfLoading = () => {( type: FETCH_SMURF_LOADING )};
+export const smurfLoading = () => ( {type: FETCH_SMURF_LOADING} );
 export const smurfLoadSuccess = data => ({
   type: FETCH_SMURF_SUCCESS,
   payload: data
---
+++ smurfs/src/reducers/App.js
@@ -0,0 +1,37 @@
+import { FETCH_SMURF_LOADING, FETCH_SMURF_SUCCESS, FETCH_SMURF_FAILED } from '../actions/App';
+
+const initialState = {
+  smurf: [],
+  error: null,
+  isFetching: false
+}
+
+function reducer(state = initialState, action) {
+  console.log('reducer', action);
+  switch (action.type) {
+    case FETCH_SMURF_LOADING:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      }
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurf: action.payload,
+        isFetching: false,
+        error: null
+      }
+    case FETCH_SMURF_FAILED:
+      return {
+        ...state,
+        smurf: [],
+        isFetching: false,
+        error: action.payload
+      }
+    default: 
+      return state;
+  }
+}
+
+export default reducer;
